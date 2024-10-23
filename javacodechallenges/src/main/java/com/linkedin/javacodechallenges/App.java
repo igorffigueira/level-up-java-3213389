896d1321434dead2ca5267fdba1b09e2b4dc3792
André Figueira <24575498+igorffigueira@users.noreply.github.com>
2024-10-23 16:17:44 +0000

    Working Solution

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -3,9 +3,35 @@ package com.linkedin.javacodechallenges;
 import java.util.Scanner;
 
 public class App {
+    private static final int minimalCharacters = 6;
 
+    /**
+     * @param password
+     * @return
+     */
     public static boolean isPasswordComplex(String password) {
-        return false;
+        if (password.length() < minimalCharacters) {
+            return false;
+        }
+
+        char[] passwordCharacters = new char[password.length()];
+        password.getChars(0, password.length(), passwordCharacters, 0);
+
+        boolean anyUppterLetter = false;
+        boolean anyLowerLetter = false;
+        boolean anyDigit = false;
+
+        for (char letter : passwordCharacters) {
+            if (Character.isUpperCase(letter)) {
+                anyUppterLetter = true;
+            } else if (Character.isLowerCase(letter)) {
+                anyLowerLetter = true;
+            } else if (Character.isDigit(letter)) {
+                anyDigit = true;
+            }
+        }
+
+        return anyUppterLetter && anyLowerLetter && anyDigit;
     }
 
     public static void main(String[] args) {
